3554368eb9e9c2d4de36e25823118cdff6de8317
lovelyladylove <lovewagemann@gmail.com>
2019-04-10 11:43:47 +0200

    First commit

--- src/index.js
+++ src/index.js
@@ -6,6 +6,53 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+document.addEventListener('DOMContentLoaded', function(){
+    let wrapper = document.getElementById('wrapper');
+    let topLayer = wrapper.querySelector('.top');
+    let handle = wrapper.querySelector('.handle');
+    let skew = 0;
+    let delta = 0;
+  
+    if(wrapper.className.indexOf('skewed') !== -1){
+      skew = 1000;
+    }
+    
+    wrapper.addEventListener('mousemove', function(e){
+      delta = (e.clientX - window.innerWidth / 2) * 0.5;
+    
+      handle.style.left = e.clientX + delta + 'px';
+  
+      topLayer.style.width= e.clientX + skew + delta + 'px';
+    });
+  });
+
+
+
+
+
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const container = document.querySelector(".container");
+
+left.addEventListener("mouseenter", () => {
+  container.classList.add("hover-left");
+});
+
+left.addEventListener("mouseleave", () => {
+  container.classList.remove("hover-left");
+});
+
+right.addEventListener("mouseenter", () => {
+  container.classList.add("hover-right");
+});
+
+right.addEventListener("mouseleave", () => {
+  container.classList.remove("hover-right");
+});
+
+
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
